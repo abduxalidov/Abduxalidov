535eb02588faa47fb1b477b1c8c7598aee88808c
abduxalidov <cryptofast717@gmail.com>
2026-03-09 21:33:23 +0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<div className="min-h-screen flex items-center justify-center text-foreground text-2xl">404 - Sahifa topilmadi</div>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
+
